d1b245ba281f8c7deb2c22c8d4430c526256b675
Yasvant kumar <yasvantkumarmca@gmail.com>
2022-10-09 22:07:58 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String args[]){
+           int a=10;
+           int b=10;
+           int c=a+b;
+           System.out.println("The sum of two number is "+c);
+    }
+    
+}
